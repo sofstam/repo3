7a5f53d287b311197bee70557cf3d3b949574846
Sofia Stamouli <91951607+sofstam@users.noreply.github.com>
2021-12-02 15:37:25 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!bin/bash
 
-docker pull genomicmedicinesweden/gms-artic-illumina:latest
+singularity pull --force /home/vagrant/containers/gms-artic-illumina.sif docker://genomicmedicinesweden/gms-artic-illumina:latest
+#singularity pull --force /home/proj/${INSTANCE}/mutant/MUTANT/mutant/externals/gms-artic/artic-ncov2019-illumina.sif docker://genomicmedicinesweden/gms-artic-illumina:latest
